2aee3ebf89a574ec75dffff6b057f341caf28597
Casey Cameron <mcasey.cameron@gmail.com>
2021-04-01 13:56:49 -0700

    addItemToCart function

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,3 +1,5 @@
+import { findById } from './utils.js';
+
 const CART = 'CART';
 
 export function getCart(){
@@ -18,4 +20,27 @@ export function getCart(){
 export function setCart(parsedCart){
     const stringyCart = JSON.stringify(parsedCart);
     localStorage.setItem(CART, stringyCart);
+}
+
+export function addItemToCart(productID){
+    //run it through getCart() to see if something is already there or if empty
+    const cart = getCart();
+
+    //match the product with the id in the cart
+    const matchingItem = findById(cart, productID);
+
+    //if true (item is in cart) increment the quantity for that item
+    if (matchingItem){
+        matchingItem.quantity++;
+    //else, create an item
+    } else {
+        const item = {
+            id: productID,
+            quantity: 1
+        };
+        //push it to the cart
+        cart.push(item);
+    }
+    //save the cart locally with the setCart() function
+    setCart(cart);
 }
