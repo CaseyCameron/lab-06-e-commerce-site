cfaea55ecb889b11f4a2a412b2e3df737ae5bc10
Casey Cameron <mcasey.cameron@gmail.com>
2021-04-02 10:27:01 -0700

    Fixed TDD test for lab 06

--- test/utils.test.js
+++ test/utils.test.js
@@ -5,28 +5,28 @@ import { thundercat } from '../product.js';
 
 const test = QUnit.test;
 
-// test('take in a thundercat object and return an li element', (expect) => {
-//     //Arrange
-//     // Set up your arguments and expectations
-//     const expected = `<li class="thundercat" style="background: lightblue none repeat scroll 0% 0%;"><p class="name">Lion-O</p><img class="image" src="../assets/lion-o.jpg"><p class="description">Intrepid leader and guiding light.</p><p class="category">Leader</p><p class="is-real">Is not a person</p><p class="cost">$50</p><button class="button">Add to cart</button></li><li class="thundercat" style="background: orange none repeat scroll 0% 0%;"><p class="name">Cheetara</p><img class="image" src="../assets/cheetara.jpg"><p class="description">Indomintable and fast.</p><p class="category">Runner</p><p class="is-real">Is not a person</p><p class="cost">$45</p><button class="button">Add to cart</button></li><li class="thundercat" style="background: cornflowerblue none repeat scroll 0% 0%;"><p class="name">Panthero</p><img class="image" src="../assets/panthero.jpg"><p class="description">Resourceful and strong.</p><p class="category">Engineer</p><p class="is-real">Is not a person</p><p class="cost">$42.5</p><button class="button">Add to cart</button></li><li class="thundercat" style="background: red none repeat scroll 0% 0%;"><p class="name">Snarf</p><img class="image" src="../assets/snarf.jpg"><p class="description">Cowardly comedic relief.</p><p class="category">Pet</p><p class="is-real">Is not a person</p><p class="cost">$35</p><button class="button">Add to cart</button></li><li class="thundercat" style="background: pink none repeat scroll 0% 0%;"><p class="name">Thundercat</p><img class="image" src="../assets/thundercat.jpg"><p class="description">Phenominal musician and bass player.</p><p class="category">Musician</p><p class="is-real">Is a person</p><p class="cost">$1000000</p><button class="button">Add to cart</button></li>`;
+test('take in a thundercat object and return an li element', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<li class="thundercat" style="background: pink none repeat scroll 0% 0%;"><p class="name">Thundercat</p><img class="image" src="../assets/thundercat.jpg"><p class="description">Phenominal bass player.</p><p class="category">Musician</p><p class="is-real">Is a person</p><p class="cost">$1000000</p><p class=\"quantity\"></p><button class=\"button\">Add to cart</button></li>`;
     
-//     //Act 
-//     // Call the function you're testing and set the result to a const
-//     const actual = createThundercat({
-//         id: '5',
-//         name: 'Thundercat',
-//         image: '../assets/thundercat.jpg',
-//         description: 'Phenominal musician and bass player.',
-//         category: 'Musician',
-//         isReal: true,
-//         color: 'pink',
-//         cost: 1000000,
-//     });
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = createThundercat({
+        id: 5,
+        name: 'Thundercat',
+        image: '../assets/thundercat.jpg',
+        description: 'Phenominal bass player.',
+        category: 'Musician',
+        isReal: true,
+        color: 'pink',
+        cost: 1000000,
+    });
 
-//     //Expect
-//     // Make assertions about what is expected versus the actual result
-//     expect.equal(actual.outerHTML, expected);
-// });
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
 
 test('take an array and check to see if the item.id matches', (expect) => {
     const expected = cart[0];
